255ea5114ec19d2ce4f4cd265db221ebf1354a3c
lizxqws <goncharov4ento@gmail.com>
2024-07-24 22:13:25 +0200

    Add section check year
    html + css

---
+++ index.js
@@ -0,0 +1,14 @@
+// const checkYearMessage = document.querySelector(".check-year__input-message");
+// const checkYearInput = document.querySelector("check-year__input-text");
+// const checkYearButton = document.querySelector("check-year__input-button");
+// const addBtn = document.querySelector('btn[data-action="add"]');
+// const checkYear = () => {
+//   if (checkYearInput.value / 4) {
+//     alert("ДА");
+//   } else {
+//     alert("Нет");
+//   }
+// };
+// addBtn.addEventListener("click", () => {
+//   checkYearButton.addEventListener("click", checkYear);
+// });
