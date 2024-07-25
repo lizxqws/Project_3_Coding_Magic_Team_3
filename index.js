9efae9ae45884ca8efc83312d4b2bb4364206fc7
lizxqws <goncharov4ento@gmail.com>
2024-07-25 21:56:43 +0200

    Add section check year
    Add js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-// const checkYearMessage = document.querySelector(".check-year__input-message");
-// const checkYearInput = document.querySelector("check-year__input-text");
-// const checkYearButton = document.querySelector("check-year__input-button");
-// const addBtn = document.querySelector('btn[data-action="add"]');
-// const checkYear = () => {
-//   if (checkYearInput.value / 4) {
-//     alert("ДА");
-//   } else {
-//     alert("Нет");
-//   }
-// };
-// addBtn.addEventListener("click", () => {
-//   checkYearButton.addEventListener("click", checkYear);
-// });
+const checkYearMessage = document.querySelector(".check-year__input-message");
+const checkYearInput = document.querySelector(".check-year__input-text");
+const checkYearButton = document.querySelector(".check-year__input-button");
+const checkYearAddBtn = document.querySelector('#btn[data-action="add"]');
+
+const checkYear = () => {
+  if (checkYearInput.value % 4) {
+    checkYearMessage.style.cssText = "color: #990000; ";
+    checkYearMessage.innerHTML = "Ви народилися не у високосний рік!";
+  } else {
+    checkYearMessage.style.cssText = "color: #039900; ";
+    checkYearMessage.innerHTML = "Ви народилися у високосний рік!";
+  }
+};
+
+checkYearAddBtn.addEventListener("click", () => {
+  checkYearButton.addEventListener("click", checkYear);
+});
