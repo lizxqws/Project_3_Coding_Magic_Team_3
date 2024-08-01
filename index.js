16b5783fb8cdb2c51b89c8ab17dac4260fd528c3
AddNiI <100078200+AddNiI@users.noreply.github.com>
2024-08-01 19:49:44 +0300

    final

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const targetNumberNumberGuess = Math.floor(Math.random() * 10) + 1;
 function checkNumberNumberGuess() {
     const userNumberNumberGuess = parseInt(document.getElementById('number-inputNumberGuess').value);
+	console.log(userNumberNumberGuess);
     const resultMessageNumberGuess = document.getElementById('result-messageNumberGuess');
+	if(isNaN(userNumberNumberGuess)){
+		alert('Введiть число!!!!')
+		resultMessageNumberGuess.textContent = '';
+		return;
+		}
     if (userNumberNumberGuess === targetNumberNumberGuess) {
         resultMessageNumberGuess.textContent = `Вітаю, ви вгадали число! (${targetNumberNumberGuess})`;
 		resultMessageNumberGuess.classList.add('ResultMGood');
