1aac29936f7a74a6d5324f15b6789919b8db2fd3
lizxqws <goncharov4ento@gmail.com>
2024-08-01 21:43:01 +0200

    add pop-up

---
+++ pop-up.js
@@ -0,0 +1,37 @@
+const closeButton = document.querySelector("#mw-close");
+const saveButton = document.querySelector("#pop-up__save");
+const nameInput = document.querySelector("#pop-up__text-input");
+const popUp = document.querySelector("#pop-up__window");
+const body = document.querySelector("body");
+
+saveButton.addEventListener("click", function () {
+  const nameValue = nameInput.value;
+
+  if (nameValue.length > 1 && nameValue.length < 15) {
+    popUp.style.display = "none";
+    body.style.overflow = "visible";
+  } else {
+    alert("Некоректно введено ім'я. Спробуйте ще раз.");
+  }
+
+  return (nameInput.value = "");
+});
+
+closeButton.addEventListener("click", function () {
+  popUp.style.display = "none";
+  body.style.overflow = "visible";
+});
+
+popUp.addEventListener("click", function (event) {
+  if (event.target === popUp) {
+    popUp.style.display = "none";
+    body.style.overflow = "visible";
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    popUp.style.display = "none";
+    body.style.overflow = "visible";
+  }
+});
