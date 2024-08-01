ac46b42e67948c10970808748845c6f097503b0e
lizxqws <goncharov4ento@gmail.com>
2024-08-01 18:41:38 +0200

    add validation

--- footer.js
+++ footer.js
@@ -1,9 +1,15 @@
 const mwClose = document.querySelector("#mw-close");
 const mwOpen = document.querySelector("#mw-open");
 const modal = document.querySelector("#modal");
+const email = document.querySelector("#footer-email");
 
 mwOpen.addEventListener("click", function () {
-  modal.style.display = "block";
+  if (email.value.includes("@")) {
+    modal.style.display = "block";
+  } else {
+    alert("Некоректно введен email. Спробуйте ще раз.");
+  }
+  return (email.value = "");
 });
 
 mwClose.addEventListener("click", function () {
